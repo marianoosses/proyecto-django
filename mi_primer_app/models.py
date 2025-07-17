818563474237050f97f6ea90d5a7f67d72f540e9
Mariano Osses <mariano_osses@hotmail.com>
2025-07-17 01:16:54 -0300

    creamos modelo familiar

---
+++ mi_primer_app/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+class Familiar(models.Model):
+    nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=50)
+    edad = models.IntegerField()
+    fecha_nacimiento = models.DateField()
+    parentesco = models.CharField(max_length=50)
+
+
+    def __str__(self):
+        return f"{self.nombre} {self.apellido} ({self.parentesco}) - Edad:{self.edad}, Nacimiento: {self.fecha_nacimiento}"
